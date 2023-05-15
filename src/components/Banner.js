20463de8740b257538536bdb4a2c7869fa7c3354
Hritik Singh <98538585+Hritik-Singh067@users.noreply.github.com>
2023-05-15 16:22:26 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 function Banner() {
   return (
@@ -9,19 +10,19 @@ function Banner() {
       <Carousel autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false} interval={4000}>
         <div >
           <img className='relative' loading='lazy' src="/banner.png" alt='Banner image 1' width={50} height={50} />
-          <a className='absolute bottom-10 right-10 text-white' href="/">VIEW MORE</a>
+          <a className='absolute bottom-10 right-10 text-white' href="/">VIEW MORE &GT;</a>
         </div>
         <div>
-          <img loading='lazy' src="/banner.png" alt='Banner image 2' width={50} height={50} />
-          <a className='absolute bottom-10 right-10 text-white' href="/">VIEW MORE</a>
+          <img loading='lazy' src="/banner.png" alt='Banner image 2' width={10} height={10} />
+          <a className='absolute bottom-10 right-10 text-white'  href="/">VIEW MORE &GT;</a>
         </div>
         <div>
           <img loading='lazy' src="/banner.png" alt='Banner image 3' width={50} height={50} />
-          <a className='absolute bottom-10 right-10 text-white' href="/">VIEW MORE</a>
+          <a className='absolute bottom-10 right-10 text-white' href="/">VIEW MORE &#62;</a>
         </div>
       </Carousel>
     </div>
-  );
+  )
 }
 
-export default Banner;
+export default Banner
